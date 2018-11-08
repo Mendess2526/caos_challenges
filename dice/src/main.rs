68315c16cd29180e828b271f432f02bf9a4778f7
Mendess2526 <pedro.mendes.26@gmail.com>
2018-11-08 20:12:46 +0000

    Add time measuring

--- dice/src/main.rs
+++ dice/src/main.rs
@@ -4,6 +4,7 @@ extern crate rand;
 use std::fs::File;
 use memmap::MmapOptions;
 use rand::Rng;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 
 fn do_it(line :&[u8]) {
@@ -27,13 +28,22 @@ fn do_it(line :&[u8]) {
     for _i in 0..rolls {
         sum += 1 + rng.gen::<u64>() % faces;
     }
-    println!("{}", sum);
+    //println!("{}", sum);
 }
 
 fn main() -> std::io::Result<()> {
+    let start_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     let file = File::open("input.txt")?;
     let memmap = unsafe { MmapOptions::new().map(&file)? };
+
+    let mid_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     memmap.split(|c| *c == 10) // *c == '\n'
         .for_each(|line| do_it(line));
+
+    let end_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let load_time = mid_time - start_time;
+    let do_time   = end_time - mid_time;
+    println!("Loadging file: {} micros", load_time.as_secs() * 1000 + load_time.subsec_nanos() as u64 / 1_000);
+    println!("Doing it:      {} micros", do_time.as_secs()   * 1000 + do_time.subsec_nanos()   as u64 / 1_000);
     Ok(())
 }
