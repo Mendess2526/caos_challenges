80a5cfd7d828b42cff38e26580e91700d509a3bc
Mendess2526 <pedro.mendes.26@gmail.com>
2019-10-07 23:31:05 +0100

    Improve paralel string buf

--- dice/par-string-buf.rs
+++ dice/par-string-buf.rs
@@ -1,18 +1,19 @@
 extern crate memmap;
 extern crate rand;
 
-use std::fs::File;
 use memmap::MmapOptions;
 use rand::Rng;
-use std::time::Instant;
+use std::fmt::Write as WriteFmt;
+use std::fs::File;
+use std::io::{IoSlice, Write};
 use std::thread;
-use std::sync::mpsc;
-use mpsc::{Sender, Receiver};
 
-fn do_it(line :&[u8], out :&mut String) {
-    if line.len() < 1 { return; }
-    let mut rolls :u64 = 0;
-    let mut faces :u64 = 0;
+fn do_it(line: &[u8], out: &mut String) {
+    if line.is_empty() {
+        return;
+    }
+    let mut rolls: u64 = 0;
+    let mut faces: u64 = 0;
     let mut pre_d = true;
     for c in line {
         if *c == b'd' {
@@ -20,57 +21,51 @@ fn do_it(line :&[u8], out :&mut String) {
             continue;
         }
         if pre_d {
-            rolls = (rolls * 10) + (*c - b'0') as u64;
-        }else{
-            faces = (faces * 10) + (*c - b'0') as u64;
+            rolls = (rolls * 10) + u64::from(*c - b'0');
+        } else {
+            faces = (faces * 10) + u64::from(*c - b'0');
         }
     }
-    let mut sum :u64 = 0;
+    let mut sum: u64 = 0;
     let mut rng = rand::thread_rng();
     for _i in 0..rolls {
         sum += 1 + rng.gen::<u64>() % faces;
     }
-    out.push_str(&format!("{}", sum));
+    let _ = writeln!(out, "{}", sum);
 }
 
 fn main() -> std::io::Result<()> {
-    let start_time = Instant::now();
     let file = File::open("input.txt")?;
     let memmap = unsafe { MmapOptions::new().map(&file)? };
 
-    let mid_time = Instant::now();
-
-    // ============== do ==================
-    let chunk_size = 2_usize.pow(19);
-    let mut threads = Vec::with_capacity(memmap.len() / chunk_size);
-    let mut index :usize = chunk_size;
-    let mut last_index :usize = 0;
-    let (tx, rx) :(Sender<String>, Receiver<String>)= mpsc::channel();
+    let chunk_size = memmap.len() / num_cpus::get();
+    let mut threads = Vec::with_capacity(num_cpus::get());
+    let mut index: usize = chunk_size;
+    let mut last_index: usize = 0;
     while index < memmap.len() {
-        while memmap[index] != b'\n' { index += 1; }
+        while memmap[index] != b'\n' {
+            index += 1;
+        }
         let slice: &'static [u8] = unsafe { &*(&memmap[last_index..index] as *const _) };
-        let tx_clone = mpsc::Sender::clone(&tx);
-        let runnable = move || {
-            let mut out = String::new();
+        threads.push(thread::spawn(move || {
+            let mut out = String::with_capacity(slice.len());
             slice
                 .split(|c| *c == b'\n')
                 .for_each(|line| do_it(line, &mut out));
-            tx_clone.send(out).unwrap();
-        };
-        threads.push(thread::spawn(runnable));
+            out
+        }));
         last_index = index;
-        index = index + chunk_size;
+        index += chunk_size;
     }
-    let mut out = String::new();
+    let mut out = String::with_capacity(memmap[last_index..].len());
     memmap[last_index..]
         .split(|c| *c == b'\n')
         .for_each(|line| do_it(line, &mut out));
-    std::mem::drop(tx);
-    println!("{}", out + &rx.iter().fold(String::new(), |x, y| x + &y));
-    for t in threads { t.join().unwrap(); }
-    // ============ done ================
-    let end_time = Instant::now();
-    eprintln!("Loading file:  {:?}", mid_time.duration_since(start_time));
-    eprintln!("Doing it:      {:?}", end_time.duration_since(mid_time));
-    Ok(())
+    let r = threads
+        .into_iter()
+        .map(|t| Vec::from(t.join().unwrap()).into_boxed_slice())
+        .chain(std::iter::once(Vec::from(out).into_boxed_slice()))
+        .collect::<Vec<Box<[u8]>>>();
+    let slices = r.iter().map(|s| IoSlice::new(&*s)).collect::<Vec<_>>();
+    std::io::stdout().lock().write_vectored(&slices).map(|_| ())
 }
